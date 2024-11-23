bee31e031fac0d68ec6c5b46eae18aabe7c6774d
Diego Miguel <dmlls@diegomiguel.me>
2024-11-23 20:10:38 +0100

    Implement tests for comissioning algorithms

---
+++ algos/test/test_genetic.py
@@ -0,0 +1,17 @@
+"""Test the genetic algorithm."""
+
+import json
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).parent.parent))
+from genetic import solution as genetic_sol
+
+SCENARIO_NUMBER: int = 3
+
+if __name__ == "__main__":
+    with open(Path(f"data/scenario_{SCENARIO_NUMBER:>02}.json"), "r") as f:
+        loaded_scenario = json.load(f)
+    genetic_sol.solve(loaded_scenario["vehicles"], loaded_scenario["customers"])
+    print("Number of vehicles:", len(loaded_scenario["vehicles"]))
+    print("Number of customers:", len(loaded_scenario["customers"]))
