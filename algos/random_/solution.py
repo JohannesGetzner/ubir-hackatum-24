61407811829836898149d10faff53121a4f4ffe1
Diego Miguel <dmlls@diegomiguel.me>
2024-11-23 20:09:40 +0100

    Implement random allocation

---
+++ algos/random_/solution.py
@@ -0,0 +1,24 @@
+"""Random taxi commissioning."""
+
+import random
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).parent.parent))
+from models import Scenario
+
+
+def solve(
+    vehicles: list[dict],
+    customers: list[dict],
+) -> list[tuple[int, int]]:
+    """Solve a taxi commission with random allocations."""
+    scenario = Scenario(vehicles, customers)
+    solution = []
+    vehicles_idx = list(scenario.indexed_vehicles)
+    customers_idx = list(scenario.indexed_customers)
+    random.shuffle(customers_idx)
+    for customer in customers_idx:
+        solution.append((random.choice(vehicles_idx), customer))
+    print(solution)
+    print("Solution cost:", scenario.calculate_cost(solution))
